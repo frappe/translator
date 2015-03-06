fe826f858c54036525359fd8a9dc41dc1b9292cc
Pratik Vyas <m@pd.io>
2015-03-06 11:39:36 +0530

    Disable migrations of translator doctype, will use mysqldump

--- translator/migrate.py
+++ translator/migrate.py
@@ -6,11 +6,11 @@ def migrate():
 	print "connecting..."
 	remote = FrappeClient("https://frappe.io", "Administrator", frappe.conf.frappe_admin_password)
 	remote.migrate_doctype("User", exclude=["Guest", "Administrator"], preprocess=remove_undesired_roles)
-	remote.migrate_doctype("Language")
-	remote.migrate_doctype("Translator App")
-	remote.migrate_doctype("Source Message")
-	remote.migrate_doctype("Translated Message")
-	remote.migrate_doctype("Translated Message Validation")
+	# remote.migrate_doctype("Language")
+	# remote.migrate_doctype("Translator App")
+	# remote.migrate_doctype("Source Message")
+	# remote.migrate_doctype("Translated Message")
+	# remote.migrate_doctype("Translated Message Validation")
 
 def remove_undesired_roles(doc):
 	doc["user_roles"] = [role for role in (doc.get("user_roles") or [])
