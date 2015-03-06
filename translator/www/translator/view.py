fc5777ba2788c0a741edfb2133fcd10df47569a2
Pratik Vyas <m@pd.io>
2015-03-06 11:40:05 +0530

    search in translated as well

--- translator/www/translator/view.py
+++ translator/www/translator/view.py
@@ -5,27 +5,27 @@ def get_context(context):
 	translated.name as translated_name, translated.translated, translated.verified, source.flagged,
 	translated.modified, translated.modified_by, user.first_name, user.last_name from `tabSource Message` source 
 	left join `tabTranslated Message` translated on (source.name=translated.source and translated.language = %s) 
-	left join tabUser user on (translated.modified_by=user.name) {condition} and source.disabled !=1 order by source.message"""
+	left join tabUser user on (translated.modified_by=user.name) where {condition} and source.disabled !=1 order by source.message"""
 
 	lang = frappe.form_dict.lang
 
 	if frappe.form_dict.search:
-		condition = "where source.message like %s"
-		condition_value = '%' + frappe.form_dict.search + '%'
+		condition = "(source.message like %s or translated.translated like %s)"
+		condition_values = ['%' + frappe.form_dict.search + '%', '%' + frappe.form_dict.search + '%']
 	
 	else:
 		c = frappe.form_dict.c or "*"
 		if c == "*":
-			condition = "where source.message REGEXP '^[^a-zA-Z]'"
-			condition_value = None
+			condition = "source.message REGEXP '^[^a-zA-Z]'"
+			condition_values = None
 
 		else:
-			condition_value = c + '%'
-			condition = "where source.message like %s"
+			condition = "source.message like %s"
+			condition_values = [c + '%']
 	
-	cond_tupple = (lang, condition_value) if condition_value else (lang,)
+	cond_tuple = tuple([lang] + condition_values) if condition_values else (lang,)
 
-	context.messages = frappe.db.sql(query.format(condition=condition), cond_tupple, as_dict=True)
+	context.messages = frappe.db.sql(query.format(condition=condition), cond_tuple, as_dict=True)
 
 	context.parents = [
 		{"title": "Community", "name":"community"},
