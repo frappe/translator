d969936a79af2bd4b3fd0748b5c1830071ccdd92
Suraj Shetty <surajshetty3416@gmail.com>
2020-01-29 11:57:41 +0530

    fix(translation): Filtering and Language label

--- translator/www/index.py
+++ translator/www/index.py
@@ -1,9 +1,13 @@
 import frappe
 
+languages = frappe.get_all("Language", fields=["language_code", "language_name"])
+
 def get_context(context):
 	context.get_info = frappe.get_attr("translator.helpers.get_info")
 	context.parents =  [{"title":"Community", "name":"community"}]
-	context.languages = frappe.get_all("Language", fields=["language_code", "language_name"])
+	context.languages = languages
+
+	context.language_labels = {lang.language_code: lang.language_name for lang in languages}
 
 	context.status_options = frappe.get_cached_value("DocField", {
 		'parent': 'Contributed Translation',
@@ -11,6 +15,8 @@ def get_context(context):
 	}, 'options').split('\n')
 
 	context.contributed_translations = frappe.get_all("Contributed Translation",
-		filters=frappe.form_dict, fields=["*"], limit_page_length=100)
+		filters=frappe._dict(frappe.form_dict),
+		fields=['language', 'source_string', 'translated_string', 'status'],
+		limit_page_length=100)
 
 	context.no_cache = 1
