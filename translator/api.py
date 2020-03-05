6f0e16a5be6dbbf0d2cd0a7710021a0f172512f5
Suraj Shetty <surajshetty3416@gmail.com>
2020-03-05 09:40:44 +0530

    refactor: Remove unwanted APIs

--- translator/api.py
+++ translator/api.py
@@ -4,40 +4,6 @@ from frappe.utils import cint
 from frappe.translate import load_lang, get_user_translations, get_messages_for_app
 
 @frappe.whitelist(allow_guest=True)
-def add_translation(data):
-	data = json.loads(data)
-	if frappe.db.get_all("Contributed Translation", filters={"source_string": data.get("source_name"), "translated_string": data.get("target_name"), "language": data.get("language")}):
-		return {
-			"message": "Already exists"
-		}
-	else:
-		contributed_translation = frappe.get_doc({
-			"doctype": "Contributed Translation",
-			"language": data.get("language"),
-			"contributor": data.get("contributor"),
-			"source_string": data.get("source_name"),
-			"translated_string": data.get("target_name"),
-			"posting_date": data.get("posting_date")
-		}).insert(ignore_permissions = True)
-		return {
-			"message": "Added to contribution list",
-			"doc_name": contributed_translation.name
-		}
-
-@frappe.whitelist(allow_guest=True)
-def translation_status(data):
-	data = json.loads(data)
-	status = frappe.db.get_value("Contributed Translation", data.get("doc_name"), "status")
-	if status:
-		return {
-			"status": status
-		}
-	else:
-		return {
-			"message": "Contributed Translation has been deleted"
-		}
-
-@frappe.whitelist(allow_guest=True)
 def add_translations(translation_map, contributor_name, contributor_email, language):
 	translation_map = json.loads(translation_map)
 	for source_text, translation_dict in translation_map.items():
