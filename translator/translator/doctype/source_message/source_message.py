4f421ad2849f67a1fb56923e74b5a18b0b88242f
Suraj Shetty <surajshetty3416@gmail.com>
2020-03-09 15:08:37 +0530

    fix: Create google translations on new source message creation

--- translator/translator/doctype/source_message/source_message.py
+++ translator/translator/doctype/source_message/source_message.py
@@ -4,7 +4,32 @@
 from __future__ import unicode_literals
 import frappe
 from frappe.model.document import Document
+from translator.data import get_translation_from_google
 
 class SourceMessage(Document):
 	def autoname(self):
 		self.name = frappe.generate_hash()
+
+	def after_insert(self):
+		frappe.enqueue_doc(self.doctype, self.name, 'create_google_translations')
+
+	def create_google_translations(self):
+		langs = frappe.db.sql_list("select name from tabLanguage")
+		try:
+			for lang in langs:
+				create_translation(lang, self)
+		except:
+			pass
+
+def create_translation(lang, source_dict):
+	t = frappe.new_doc('Translated Message')
+	t.translation_type = 'Google Translated'
+	t.language = lang
+	t.source = source_dict.name
+	try:
+		t.translated = get_translation_from_google(lang, source_dict.message)
+		t.save()
+	except requests.exceptions.HTTPError:
+		print("Skipping {0}".format(lang))
+	except frappe.exceptions.ValidationError:
+		pass
