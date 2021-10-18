381ef4901554383d2108674b4376534133c9f754
Mohammad Hasnain <hasnain2808@gmail.com>
2021-10-18 17:50:16 +0530

    feat: clone in temp directory

--- translator/translator/doctype/translator_app/translator_app.py
+++ translator/translator/doctype/translator_app/translator_app.py
@@ -2,8 +2,64 @@
 # For license information, please see license.txt
 
 from __future__ import unicode_literals
+import requests
 import frappe
+import os
+import tarfile
 from frappe.model.document import Document
+from frappe.utils import  get_site_path, random_string
 
 class TranslatorApp(Document):
-	pass
+	def validate(self):
+		frappe.enqueue(
+			self.create_source_strings,
+			name=self.name,
+			enqueue_after_commit=True
+		)
+		self.status = 'Queued'
+
+	def create_source_strings(self, name):
+		frappe.db.set_value('Translator App', name, 'status', 'Validating Sources')
+
+		for source in self.sources:
+			r = requests.request('HEAD', f"https://api.github.com/repos/{self.owner}/{self.name}/tarball/{source.source}")
+			if r.status_code != 200:
+				frappe.db.set_value('Translator App', name, 'status', 'Failed')
+				log = f'Source {source.source} Not Found. Make Sure that it is a branch or Tag'
+				frappe.db.set_value('Translator App', name, 'log', log)
+				return
+
+		frappe.db.set_value('Translator App', name, 'status', 'Getting Code')
+
+		for source in self.sources:
+
+
+			r = requests.get( f"https://api.github.com/repos/{self.owner}/{self.name}/tarball/{source.source}", stream=True)
+
+			self._prepare_clone_directory(source.source)
+
+			file = tarfile.open(fileobj=r.raw, mode="r|gz")
+			file.extractall(path=self.clone_directory)
+
+		frappe.db.set_value('Translator App', name, 'status', 'Successful')
+
+
+
+	def _prepare_clone_directory(self, source):
+		clone_directory  = get_site_path(frappe.db.get_single_value("Translation Settings", "clone_directory"))
+		if not os.path.exists(clone_directory):
+			os.mkdir(clone_directory)
+
+		app_directory = os.path.join(clone_directory, self.app_name)
+		if not os.path.exists(app_directory):
+			os.mkdir(app_directory)
+
+		source_directory = os.path.join(app_directory, source)
+		if not os.path.exists(source_directory):
+			os.mkdir(source_directory)
+
+		self.clone_directory = os.path.join(
+			clone_directory, self.app_name, source, random_string(7)
+		)
+		if not os.path.exists(source_directory):
+			os.mkdir(self.clone_directory)
