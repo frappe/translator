add79011c86f748232bbb72a778dd5ff06c71e1d
Mohammad Hasnain <hasnain2808@gmail.com>
2021-10-22 01:40:27 +0530

    wip: get messages from clone
    feat: add positions to db

--- translator/translator/doctype/translator_app/translator_app.py
+++ translator/translator/doctype/translator_app/translator_app.py
@@ -8,6 +8,8 @@ import os
 import tarfile
 from frappe.model.document import Document
 from frappe.utils import  get_site_path, random_string
+from frappe.utils import update_progress_bar
+from frappe.core.utils import find
 
 class TranslatorApp(Document):
 	def validate(self):
@@ -50,6 +52,9 @@ class TranslatorApp(Document):
 			# with open('messages.txt', 'w') as fp:
 			# 	fp.write(str(len(messages)))
 
+			formatted_messages = get_formatted_messages(messages, self.app_name, source.source)
+			import_source_messages(formatted_messages, self.name)
+
 		frappe.db.set_value('Translator App', name, 'status', 'Successful')
 
 
@@ -72,3 +77,78 @@ class TranslatorApp(Document):
 		)
 		if not os.path.exists(clone_directory):
 			os.mkdir(self.clone_directory)
+
+
+
+def import_source_messages(message_map, name):
+	"""Import messages from apps listed in **Translator App** as **Source Message**"""
+	l = len(message_map)
+	# frappe.db.set_value('Translator App', name, 'log', message_map)
+
+	frappe.db.sql("UPDATE `tabSource Message` SET `disabled`=1")
+	for i, ((message, context), positions) in enumerate(message_map.items()):
+		# used SQL so as to make message comparision case sensitive
+
+		# try:
+
+		source_message = frappe.db.sql("""
+			SELECT `name`
+			FROM `tabSource Message`
+			WHERE `message` = BINARY %s AND coalesce(`tabSource Message`.context, '') = %s
+			LIMIT 1
+		""", (message, context), as_dict=1)
+		
+
+		source_message = source_message[0] if source_message else None
+		if source_message:
+			d = frappe.get_doc("Source Message", source_message['name'])
+			d.disabled = 0
+			positions = get_postions_to_save(d.positions, positions)
+		else:
+			d = frappe.new_doc('Source Message')
+			d.message = message
+			d.context = context
+		d.set('positions', positions)
+
+		d.save(ignore_version=True, ignore_permissions=True)
+		# except:
+		# 	pass
+				# log = str(frappe.db.get_value('Translator App', name, 'log')) + str(message) + context
+				# frappe.db.set_value('Translator App', name, 'log', log)
+
+		update_progress_bar("Importing messages", i, l)
+
+def get_postions_to_save(old_positions, new_positions):
+	final_positions = []
+	for row in new_positions:
+		old_row = find(old_positions, lambda d: d.position == row['position'])
+		if old_row:
+			old_row.update(row)
+			final_positions.append(old_row)
+			old_positions.remove(old_row)
+		else:
+			final_positions.append(row)
+	return final_positions
+
+def get_formatted_messages(messages, app, app_version):
+	message_map = frappe._dict({})
+
+		# messages structure
+		# [(position, source_text_1, context, line_no), (position, source_text_2)]
+	for message_data in messages:
+		if not message_data: continue
+		position = message_data[0]
+		message = message_data[1]
+		context = message_data[2] or '' if len(message_data) > 2 else ''
+		line_no = message_data[3] or 0 if len(message_data) == 4 else 0
+		position_dict = frappe._dict({
+			'position': position,
+			'line_no': line_no,
+			'app': app,
+			'app_version': app_version
+		})
+		if not message_map.get((message, context)):
+			message_map[(message, context)] = [position_dict]
+		else:
+			message_map[(message, context)].append(position_dict)
+	return message_map
